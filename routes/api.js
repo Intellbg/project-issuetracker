1318e609e1d8a8812e20555f2f0fe2a1c4b13118
Nicolas Burbano <rnbp@Nicolass-MacBook-Pro.local>
2024-02-22 13:47:59 -0500

    add post

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,14 @@
 'use strict';
+// assigned_to and status_text.
+projects=[]
+REQUIERE_FIELDS=["issue_title", "issue_text", "created_by"]
+function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = Math.random() * 16 | 0;
+    const v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
 
 module.exports = function (app) {
 
@@ -6,12 +16,25 @@ module.exports = function (app) {
   
     .get(function (req, res){
       let project = req.params.project;
-      
     })
     
     .post(function (req, res){
       let project = req.params.project;
-      
+      if(! REQUIERE_FIELDS.every(attribute => req.body.hasOwnProperty(attribute))){
+        return res.code(401).json({error:'required field(s) missing'})
+      }
+      project={
+        issue_title:req.body.get("issue_title"),
+        issue_text:req.body.get("issue_text"),
+        created_by:req.body.get("created_by"),
+        created_on:new Date().getTime(),
+        updated_on:new Date().getTime(),
+        assigned_to:req.body.get("assigned_to"),
+        status_text:req.body.get("status_text"),
+        open:true,
+        _id:generateUUID()
+
+      }
     })
     
     .put(function (req, res){
