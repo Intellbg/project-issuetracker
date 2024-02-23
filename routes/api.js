4ac2e9bd109d399c97e6a8736315566decfe58a0
Roberto Burbano <nicolas.rnbp@gmail.com>
2024-02-23 12:58:49 +0000

    finish

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
 'use strict';
 // assigned_to and status_text.
-projects=[]
-REQUIERE_FIELDS=["issue_title", "issue_text", "created_by"]
+let projects = []
+const REQUIERE_FIELDS = ["issue_title", "issue_text", "created_by"]
+const OPTIONAL_FIELDS = ["assigned_to", "status_text"]
 function generateUUID() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     const r = Math.random() * 16 | 0;
     const v = c === 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
@@ -13,38 +14,88 @@ function generateUUID() {
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      let project = req.params.project;
-    })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      if(! REQUIERE_FIELDS.every(attribute => req.body.hasOwnProperty(attribute))){
-        return res.code(401).json({error:'required field(s) missing'})
-      }
-      project={
-        issue_title:req.body.get("issue_title"),
-        issue_text:req.body.get("issue_text"),
-        created_by:req.body.get("created_by"),
-        created_on:new Date().getTime(),
-        updated_on:new Date().getTime(),
-        assigned_to:req.body.get("assigned_to"),
-        status_text:req.body.get("status_text"),
-        open:true,
-        _id:generateUUID()
 
+    .get(function (req, res) {
+      let project = req.params.project;
+      let querry_params = req.query;
+      let filtered_projects = projects.filter(item => item.project == project )
+      if (querry_params) {
+        filtered_projects = filtered_projects.filter(item => {
+          for (const key in querry_params) {
+            if (item.hasOwnProperty(key)) {
+              if (item[key] !== undefined && item[key] !== querry_params[key]) {
+                return false;
+              }
+            }
+          }
+          return true;
+        });
       }
+      return res.json(filtered_projects)
     })
-    
-    .put(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
-      
+      if (!REQUIERE_FIELDS.every(attribute => req.body.hasOwnProperty(attribute))) {
+        return res.json({ error: "required field(s) missing" })
+      }
+      project = {
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        created_on: new Date().toISOString(),
+        updated_on: new Date().toISOString(),
+        assigned_to: req.body.assigned_to || "",
+        status_text: req.body.status_text || "",
+        open: true,
+        project: project,
+        _id: generateUUID()
+      }
+      projects.push(project)
+      return res.status(201).json(project)
     })
-    
-    .delete(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
+      const { _id } = req.body
+      if (!_id) {
+        return res.json({ error: 'missing _id' })
+      }
+      if (
+        (!REQUIERE_FIELDS.some(attribute => req.body.hasOwnProperty(attribute))) &
+        (!OPTIONAL_FIELDS.some(attribute => req.body.hasOwnProperty(attribute)))
+      ) {
+        return res.json({ error: 'no update field(s) sent', '_id': _id })
+      }
+      let selected_project = projects.filter(item => item._id === _id )
+      if (selected_project.length==0) {
+        return res.json({ error: 'could not update', '_id': _id })
+      }
+      projects = projects.map(obj => {
+        if (obj._id === _id) {
+          Object.keys(req.body).forEach(key => {
+            obj[key]=req.body[key]
+          });
+          obj.updated_on = new Date().toISOString()
+        }
+        return obj; 
+      });
+      selected_project = projects.filter(item => item._id === _id )
+      return res.json({ result: 'successfully updated', '_id': _id })
+    })
+
+    .delete(function (req, res) {
+      let project = req.params.project;
+      const { _id } = req.body
+      if (!_id) {
+        return res.json({ error: 'missing _id' })
+      }
+      let selected_project = projects.filter(item => item._id === _id )
+      if (selected_project.length==0) {
+        return res.json({ error: 'could not delete', '_id': _id })
+      }
+      projects=projects.filter(obj => obj._id != _id);
+      return res.json({ result: 'successfully deleted', '_id': _id })
     });
-    
+
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -4,7 +4,227 @@ const assert = chai.assert;
 const server = require('../server');
 
 chai.use(chaiHttp);
-
-suite('Functional Tests', function() {
-  
+let toUpdateProject = "5871dda29faedc3491ff93bb";
+suite('Functional Tests', function () {
+    suite('POST', function () {
+        test('All', function (done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .post('/api/issues/passe')
+                .send({
+                    issue_title: "No prende",
+                    issue_text: "Al reiniciar el programa no se ejecuta",
+                    created_by: "Un Gato",
+                    assigned_to: "David",
+                    status_text: "No iniciado",
+                })
+                .end(function (err, res) {
+                    toUpdateProject = res.body['_id']
+                    assert.equal(res.status, 201);
+                    assert.equal(res.body['issue_title'], "No prende");
+                    assert.equal(res.body['issue_text'], "Al reiniciar el programa no se ejecuta",);
+                    assert.equal(res.body['created_by'], "Un Gato");
+                    assert.equal(res.body['assigned_to'], "David");
+                    assert.equal(res.body['status_text'], "No iniciado");
+                    done();
+                });
+        });
+        test('Only Required', function (done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .post('/api/issues/passe')
+                .send({
+                    issue_title: "No prende",
+                    issue_text: "Al reiniciar el programa no se ejecuta",
+                    created_by: "Otro Gato",
+                })
+                .end(function (err, res) {
+                    assert.equal(res.status, 201);
+                    assert.equal(res.body['issue_title'], "No prende");
+                    assert.equal(res.body['issue_text'], "Al reiniciar el programa no se ejecuta",);
+                    assert.equal(res.body['created_by'], "Otro Gato");
+                    assert.equal(res.body['assigned_to'], "");
+                    assert.equal(res.body['status_text'], "");
+                    done();
+                });
+        });
+        test('Missing', function (done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .post('/api/issues/passe')
+                .send({
+                    issue_title: "No prende",
+                    created_by: "Un Gato",
+                    assigned_to: "David",
+                    status_text: "No iniciado",
+                })
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body['error'], 'required field(s) missing');
+                    done();
+                });
+        });
+    });
+    suite('GET', function () {
+        test('All', function (done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .get('/api/issues/passe')
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.length, 2);
+                    done();
+                });
+        });
+        test('One', function (done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .get('/api/issues/passe?created_by=Un Gato')
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.length, 1);
+                    done();
+                });
+        });
+        test('Multiple', function (done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .get('/api/issues/passe?created_by=Un Gato&issue_title=paldea')
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.length, 0);
+                    done();
+                });
+        });
+    });
+    suite('UPDATE', function () {
+        test('ONE', function (done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .put('/api/issues/passe')
+                .send({
+                    _id: toUpdateProject,
+                    issue_title: "Nuevo Titulo"
+                })
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body['result'], 'successfully updated')
+                    assert.equal(res.body['_id'], toUpdateProject)
+                    done();
+                });
+        });
+        test('Multiple', function (done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .put('/api/issues/passe')
+                .send({
+                    _id: toUpdateProject,
+                    issue_title: "Nuevo Titulo2",
+                    issue_text: "Ocourrio un nuevo lio",
+                })
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body['result'], 'successfully updated');
+                    assert.equal(res.body['_id'], toUpdateProject);
+                    done();
+                });
+        });
+        test('Missing id', function (done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .put('/api/issues/passe')
+                .send({
+                    issue_title: "Nuevo Titulo2",
+                    issue_text: "Ocourrio un nuevo lio",
+                })
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body['error'], "missing _id");
+                    done();
+                });
+        });
+        test('Empty', function (done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .put('/api/issues/passe')
+                .send({
+                    _id: toUpdateProject,
+                })
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body['error'], 'no update field(s) sent');
+                    assert.equal(res.body['_id'], toUpdateProject);
+                    done();
+                });
+        });
+        test('Invalid', function (done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .put('/api/issues/passe')
+                .send({
+                    _id: "2"
+                })
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body['error'], 'no update field(s) sent')
+                    assert.equal(res.body['_id'], '2')
+                    done();
+                });
+        });
+    });
+    suite('DELETE', function () {
+        test('success', function (done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .delete('/api/issues/passe')
+                .send({
+                    _id: toUpdateProject,
+                })
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body['result'], 'successfully deleted');
+                    assert.equal(res.body['_id'], toUpdateProject);
+                    done();
+                });
+        });
+        test('invalid', function (done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .delete('/api/issues/passe')
+                .send({
+                    _id: toUpdateProject,
+                })
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body["error"], 'could not delete');
+                    assert.equal(res.body["_id"], toUpdateProject);
+                    done();
+                });
+        });
+        test('missing', function (done) {
+            chai
+                .request(server)
+                .keepOpen()
+                .delete('/api/issues/passe')
+                .send({})
+                .end(function (err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body['error'], 'missing _id')
+                    done();
+                });
+        });
+    });
 });
